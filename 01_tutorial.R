2ab4fbabe77f57874562b45160c7af2ba66e1628
Jimmy Glenn <jimmykglenn@gmail.com>
2018-01-02 12:46:20 -0600

    my first commit

---
+++ 01_tutorial.R
@@ -0,0 +1,8 @@
+httr::BROWSE('http://r-pkgs.had.co.nz/git.html')
+
+# run in terminal
+git config --global user.name "Jimmy Glenn"
+git config --global user.email "jimmykglenn@gmail.com"
+git config --global --list
+
+giti init
